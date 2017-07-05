3be2a2a633edf82f5f4a29f0ada5c32dc05115f0
ken <westdorp@gmail.com>
2017-07-04 18:12:49 -0700

    Initial Commit

---
+++ Meme Me/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meme Me
+//
+//  Created by Ken Westdorp on 7/4/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
